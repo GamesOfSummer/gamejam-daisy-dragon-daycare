5386cb0f663e115067dc32934f205f0795127185
Summer <summer@summertime-development.com>
2020-04-20 21:21:11 -0500

    fixed facing camera script

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour {
 
-        public float Ease = .15f;
+    public float Ease = .15f;
 
-        Transform _transform;
+    Transform _transform;
 
-        void Awake()
-        {
-            _transform = transform;
-        }
-
-        void Update()
-        {
-          
-            var screenPoint = Camera.main.WorldToScreenPoint(_transform.localPosition);
-            var offset = new Vector2(screenPoint.x, screenPoint.y);
-            var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-            _transform.rotation = Quaternion.Slerp(_transform.rotation, Quaternion.Euler(0, -angle, 0), Ease);
-        }
-
-
-
-
+    void Awake () {
+        _transform = transform;
     }
 
+    // void Update () {
+
+    //     var screenPoint = Camera.main.WorldToScreenPoint (_transform.localPosition);
+    //     var offset = new Vector2 (screenPoint.x, screenPoint.y);
+    //     var angle = Mathf.Atan2 (offset.y, offset.x) * Mathf.Rad2Deg;
+    //     _transform.rotation = Quaternion.Slerp (_transform.rotation, Quaternion.Euler (0, -angle, 0), Ease);
+
+    // }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate () {
+        transform.LookAt (transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+
+}
