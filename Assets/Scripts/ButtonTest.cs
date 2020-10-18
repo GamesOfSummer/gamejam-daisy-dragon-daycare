0882c770e0c90e6b2ba0b4ab932ea74234200ee1
neoskedar <aaron.m.oliver1@gmail.com>
2020-10-18 13:04:54 -0700

    Added new UI Buttons
    Added new UI Buttons

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+        private Animator anim;
+
+    
+    public bool animateFood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ public bool animation_bool;
+ void Update () {
+
+     }
+
+    public void PlayAnimation() {
+ gameObject.GetComponent<Animation>().Play ("FoodSlideOut");
+     }
+}
